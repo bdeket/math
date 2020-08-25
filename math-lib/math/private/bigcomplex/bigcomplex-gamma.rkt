a724d3979e62536783f01a8adb3f020199a0c862
bdeket <bedeke@hotmail.com>
2020-08-25 17:15:47 +0200

    bcgamma: more precise precision jump

--- math-lib/math/private/bigcomplex/bigcomplex-gamma.rkt
+++ math-lib/math/private/bigcomplex/bigcomplex-gamma.rkt
@@ -9,14 +9,18 @@
 
 (: bcgamma (Bigcomplex -> Bigcomplex))
 (define (bcgamma z)
+  (define (inner [z : Bigcomplex]) : Bigcomplex
+    (cond
+      [(bf< (bcreal-part z) 0.bf)
+       (bc/ pi.bc
+            (bc* (bcsin (bc* pi.bc z))
+                 (inner (bc- 1.bc z))))]
+      [else
+       (bcexp (stirling z))]))
+  
   (bccopy
-   (parameterize ([bc-precision (+ (bc-precision) 20)])
-     (cond
-       [(bf< (bcreal-part z) 0.bf)
-        (bc/ pi.bc
-             (bc* (bcsin (bc* pi.bc z))
-                  (bcgamma (bc- 1.bc z))))]
-       [else (bcexp (stirling z))]))))
+   (parameterize ([bc-precision (* 2 (bc-precision))])
+     (inner z))))
 
 ;; calculated in logform
 (define (stirling [z : Bigcomplex]) : Bigcomplex
@@ -49,9 +53,10 @@
   (cond
     [(bf< m (bf r))
      (define z+r (bc+ z (bc r)))
+     (define lg (inner z+r))
      (bccopy
       (parameterize ([bc-precision (+ p 20)])
-        (bc- (inner z+r)
+        (bc- lg
              (for/fold ([s : Bigcomplex 0.bc]
                         [z+ : Bigcomplex z]
                         #:result s)
